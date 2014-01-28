93208abdf4adb3e4613884d5a2ecc138f6e6b247
Marius Hoch <hoo@online.de>
2014-01-28 19:11:31 +0100

    Make build.sh a little faster
    Change-Id: I138f234c629f3fba47cccf7274d659e37ffeda7a

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
+branch=mw1.23-wmf11
 
 rm -rf Wikibase/
 
-git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/Wikibase.git
+git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/Wikibase.git -b $branch --depth 1
 
 cd Wikibase
-git checkout -b mw1.23-wmf11 origin/mw1.23-wmf11
 git submodule update --init --recursive
 composer install --prefer-dist --no-dev -o
 rm .gitignore
