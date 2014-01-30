7a471b7e5901a63e378f7dee4811702b7ffaf6fc
aude <aude.wiki@gmail.com>
2014-01-29 19:23:03 -0500

    restructure to match organization of grunt build
    Change-Id: I57f2f973b0191c195444c02b6749ec80954786cd

--- build.sh
+++ build.sh
@@ -1,12 +1,3 @@
 #! /bin/bash
 
-rm -rf Wikibase/
-
-git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/Wikibase.git
-
-cd Wikibase
-git checkout -b mw1.23-wmf11 origin/mw1.23-wmf11
-git submodule update --init --recursive
-composer install --prefer-dist --no-dev -o
-rm .gitignore
-rm .gitmodules
+composer update
