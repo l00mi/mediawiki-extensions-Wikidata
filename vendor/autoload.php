b184bed8ce17675bf32b6b9934ffe62794222f46
Marius Hoch <hoo@online.de>
2014-01-31 02:14:39 +0100

    Update Wikibase
    Change-Id: Ice5a07212bca6a9b30734ed57427e258f01eefbc

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit21506b6a0fb3aa17ed87ba5eec11a81c::getLoader();
+return ComposerAutoloaderInitca1cf753f323c06d02cc6cbc9ca2c786::getLoader();
