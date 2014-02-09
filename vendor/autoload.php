5217a960aa1b68fa61c6d9ca40080d7cb707a0de
WikidataBuilder <wikidata-services@wikimedia.de>
2014-02-09 10:00:19 +0000

    New Wikidata Build
    Change-Id: Iec69da25c4c1cca4273ca76a1121ace0ad41bbe4

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit791f07d9e91faa2e66936ef423f89085::getLoader();
+return ComposerAutoloaderInit93ce8a1408b888dbb75716c438ba75bc::getLoader();
