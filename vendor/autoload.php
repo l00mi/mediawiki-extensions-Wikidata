c4a0b3899625d3aeb34b2e271a9807f11888e081
aude <aude.wiki@gmail.com>
2014-02-24 17:54:20 +0100

    Update Wikibase and ValueView
    Change-Id: I3d474e22b5ab16230d91986bde96c21bf2b4c63c

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit7315ee3a33e1fda35c029578cd3f0db1::getLoader();
+return ComposerAutoloaderInit3f9eec0889d9e95b2895f59917d9d237::getLoader();
