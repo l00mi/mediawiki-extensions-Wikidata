b40d26033674919a0eecf70f2187309263c57b1f
Marius Hoch <hoo@online.de>
2014-10-17 02:06:24 +0200

    Update Wikibase to wmf/1.25wmf4 HEAD
    Change-Id: I5e392ec6201d02281766f9dfc5d1f6739108d05a

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -21,6 +21,6 @@ return array(
     $baseDir . '/extensions/ValueView/ValueView.php',
     $vendorDir . '/data-values/data-types/DataTypes.php',
     $vendorDir . '/data-values/validators/Validators.php',
-    $baseDir . '/extensions/Wikibase/Wikibase.composer.php',
     $baseDir . '/extensions/WikimediaBadges/WikimediaBadges.php',
+    $baseDir . '/extensions/Wikibase/Wikibase.composer.php',
 );
