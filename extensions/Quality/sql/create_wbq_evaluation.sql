459cc409543aed3116136a31a8fb1c4aa754da6a
Marius Hoch <hoo@online.de>
2015-07-06 07:07:50 +0200

    Update WikibaseQuality and WikibaseQualityConstraint
    Change-Id: I07699e7c701e471995443aaa3710e9e4b2874ccd

--- extensions/Quality/sql/create_wbq_evaluation.sql
+++
@@ -1,7 +0,0 @@
-	CREATE TABLE IF NOT EXISTS /*_*/wbq_evaluation (
-			  special_page_id               int               NOT NULL,
-			  entity_id                     VARCHAR(15)       NOT NULL,
-			  insertion_timestamp           int               NOT NULL,
-			  reference_timestamp           int               DEFAULT NULL,
-			  result_string                 TEXT              DEFAULT NULL
-			) /*$wgDBTableOptions*/;
