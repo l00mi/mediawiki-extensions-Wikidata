f1e734a53636d5bfd8b6d7207dea86fd0d918a7d
WikidataBuilder <wikidata-services@wikimedia.de>
2015-10-04 10:00:23 +0000

    New Wikidata Build - 2015-10-04T10:00:01+0000
    Change-Id: I2e7f35d6b80d41bb9661aade4535e4b5c4668ef5

--- extensions/ExternalValidation/includes/CrossCheck/Comparer/TimeValueComparer.php
+++ extensions/ExternalValidation/includes/CrossCheck/Comparer/TimeValueComparer.php
@@ -2,17 +2,17 @@
 
 namespace WikibaseQuality\ExternalValidation\CrossCheck\Comparer;
 
-use InvalidArgumentException;
-use DateInterval;
 use DataValues\DataValue;
 use DataValues\TimeValue;
+use DateInterval;
+use InvalidArgumentException;
+use MWTimestamp;
+use TimestampException;
 use ValueParsers\ParserOptions;
 use ValueParsers\ValueParser;
 use Wikibase\Repo\Parsers\TimeParserFactory;
 use WikibaseQuality\ExternalValidation\CrossCheck\Result\ComparisonResult;
 use WikibaseQuality\ExternalValidation\DumpMetaInformation\DumpMetaInformation;
-use MWTimestamp;
-use TimestampException;
 
 /**
  * @package WikibaseQuality\ExternalValidation\CrossCheck\Comparer
@@ -26,22 +26,31 @@ class TimeValueComparer implements DataValueComparer {
 	 *
 	 * @param DataValue $value
 	 * @param DataValue $comparativeValue
+	 *
+	 * @throws InvalidArgumentException
 	 * @return string
 	 */
 	public function compare( DataValue $value, DataValue $comparativeValue ) {
-		if( !$this->canCompare( $value, $comparativeValue ) ) {
+		if ( !$this->canCompare( $value, $comparativeValue ) ) {
 			throw new InvalidArgumentException( 'Given values can not be compared using this comparer.' );
 		}
+
+		/**
+		 * @var TimeValue $value
+		 * @var TimeValue $comparativeValue
+		 */
 
 		$result = ComparisonResult::STATUS_MISMATCH;
 
 		try {
-			$localTimestamp = new MWTimestamp(substr($value->getTime(), 1));
-			$externalTimestamp = new MWTimestamp(substr($comparativeValue->getTime(), 1));
-			$diff = $localTimestamp->diff( $externalTimestamp );
+			// FIXME: MWTimestamp does not support years with more than 4 digits!
+			$localTimestamp = new MWTimestamp( substr( $value->getTime(), 1 ) );
+			$externalTimestamp = new MWTimestamp( substr( $comparativeValue->getTime(), 1 ) );
+			$diff = $localTimestamp->diff( $externalTimestamp, true );
 
 			if ( $value->getPrecision() === $comparativeValue->getPrecision()
-					&& $this->resultOfDiffWithPrecision( $diff, $value->getPrecision() ) ) {
+				&& $this->resultOfDiffWithPrecision( $diff, $value->getPrecision() )
+			) {
 				$result = ComparisonResult::STATUS_MATCH;
 			} elseif (
 				$this->resultOfDiffWithPrecision(
@@ -51,7 +60,8 @@ class TimeValueComparer implements DataValueComparer {
 			) {
 				$result = ComparisonResult::STATUS_PARTIAL_MATCH;
 			}
-		} catch ( TimestampException $e ){ }
+		} catch ( TimestampException $ex ) {
+		}
 
 		return $result;
 	}
@@ -64,9 +74,13 @@ class TimeValueComparer implements DataValueComparer {
 	 *
 	 * @return bool
 	 */
-	private function resultOfDiffWithPrecision( $diff, $precision ) {
+	private function resultOfDiffWithPrecision( DateInterval $diff, $precision ) {
 		$result = true;
+
 		switch ( $precision ) {
+			case TimeValue::PRECISION_SECOND:
+				$result = $result && $diff->s === 0;
+				// Fall through with no break/return. This is critical for this algorithm.
 			case TimeValue::PRECISION_MINUTE:
 				$result = $result && $diff->i === 0;
 			case TimeValue::PRECISION_HOUR:
@@ -76,37 +90,35 @@ class TimeValueComparer implements DataValueComparer {
 			case TimeValue::PRECISION_MONTH:
 				$result = $result && $diff->m === 0;
 			case TimeValue::PRECISION_YEAR:
-				$result = $result && $diff->y === 0;
-			case TimeValue::PRECISION_10a:
-				$result = $result && $diff->y < 10;
-			case TimeValue::PRECISION_100a:
-				$result = $result && $diff->y < 100;
-			case TimeValue::PRECISION_ka:
-				$result = $result && $diff->y < 1000;
-			case TimeValue::PRECISION_10ka:
-				$result = $result && $diff->y < 10000;
-			case TimeValue::PRECISION_100ka:
-				$result = $result && $diff->y < 100000;
-			case TimeValue::PRECISION_Ma:
-				$result = $result && $diff->y < 1000000;
-			case TimeValue::PRECISION_10Ma:
-				$result = $result && $diff->y < 10000000;
-			case TimeValue::PRECISION_100Ma:
-				$result = $result && $diff->y < 100000000;
-			case TimeValue::PRECISION_Ga:
-				$result = $result && $diff->y < 1000000000;
-				break;
+				return $result && $diff->y === 0;
+			case TimeValue::PRECISION_YEAR10:
+				return $diff->y < 10;
+			case TimeValue::PRECISION_YEAR100:
+				return $diff->y < 100;
+			case TimeValue::PRECISION_YEAR1K:
+				return $diff->y < 1000;
+			case TimeValue::PRECISION_YEAR10K:
+				return $diff->y < 10000;
+			case TimeValue::PRECISION_YEAR100K:
+				return $diff->y < 100000;
+			case TimeValue::PRECISION_YEAR1M:
+				return $diff->y < 1000000;
+			case TimeValue::PRECISION_YEAR10M:
+				return $diff->y < 10000000;
+			case TimeValue::PRECISION_YEAR100M:
+				return $diff->y < 100000000;
+			case TimeValue::PRECISION_YEAR1G:
+				return $diff->y < 1000000000;
 			default:
-				$result = false;
+				return false;
 		}
-
-		return $result;
 	}
 
 	/**
 	 * @see DataValueComparerBase::getExternalValueParser
 	 *
 	 * @param DumpMetaInformation $dumpMetaInformation
+	 *
 	 * @return ValueParser
 	 */
 	protected function getExternalValueParser( DumpMetaInformation $dumpMetaInformation ) {
@@ -122,6 +134,7 @@ class TimeValueComparer implements DataValueComparer {
 	 *
 	 * @param DataValue $value
 	 * @param DataValue $comparativeValue
+	 *
 	 * @return bool
 	 */
 	public function canCompare( DataValue $value, DataValue $comparativeValue ) {
--- extensions/ExternalValidation/tests/phpunit/CrossCheck/Comparer/TimeValueComparerTest.php
+++ extensions/ExternalValidation/tests/phpunit/CrossCheck/Comparer/TimeValueComparerTest.php
@@ -20,73 +20,122 @@ use WikibaseQuality\ExternalValidation\CrossCheck\Result\ComparisonResult;
 class TimeValueComparerTest extends DataValueComparerTestBase {
 
 	public function comparableProvider() {
+		$g = TimeValue::CALENDAR_GREGORIAN;
+
 		return array(
 			array(
-				new TimeValue( '+0000000000001955-03-11T00:00:00Z', 0, 0, 0, 11, 'http://www.wikidata.org/entity/Q1985727' ),
-				new TimeValue( '+0000000000001955-03-11T00:00:00Z', 0, 0, 0, 11, 'http://www.wikidata.org/entity/Q1985727' )
+				new TimeValue( '+1955-03-11T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_DAY, $g ),
+				new TimeValue( '+1955-03-11T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_DAY, $g )
 			)
 		);
 	}
 
 	public function nonComparableProvider() {
+		$g = TimeValue::CALENDAR_GREGORIAN;
+
 		return array(
 			array(
 				new MonolingualTextValue( 'de', 'foobar' ),
-				new TimeValue( '+0000000000001955-03-11T00:00:00Z', 0, 0, 0, 11, 'http://www.wikidata.org/entity/Q1985727' )
+				new TimeValue( '+1955-03-11T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_DAY, $g )
 			),
 			array(
 				QuantityValue::newFromNumber( 42 ),
-				new TimeValue( '+0000000000001955-03-11T00:00:00Z', 0, 0, 0, 11, 'http://www.wikidata.org/entity/Q1985727' )
+				new TimeValue( '+1955-03-11T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_DAY, $g )
 			),
 			array(
-				new TimeValue( '+0000000000001955-03-11T00:00:00Z', 0, 0, 0, 11, 'http://www.wikidata.org/entity/Q1985727' ),
+				new TimeValue( '+1955-03-11T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_DAY, $g ),
 				new StringValue( 'foobar' )
 			),
 			array(
-				new TimeValue( '+0000000000001955-03-11T00:00:00Z', 0, 0, 0, 11, 'http://www.wikidata.org/entity/Q1985727' ),
+				new TimeValue( '+1955-03-11T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_DAY, $g ),
 				QuantityValue::newFromNumber( 42 )
 			)
 		);
 	}
 
 	public function comparisonProvider() {
-		$localValue1955 = new TimeValue( '+0000000000001955-03-11T00:00:00Z', 0, 0, 0, 11, 'http://www.wikidata.org/entity/Q1985727' );
-		$localValue2015 = new TimeValue( '+0000000000002015-00-00T00:00:00Z', 0, 0, 0, 9, 'http://www.wikidata.org/entity/Q1985727' );
-		$localValue2016 = new TimeValue( '+0000000000002016-03-00T00:00:00Z', 0, 0, 0, 10, 'http://www.wikidata.org/entity/Q1985727' );
+		$g = TimeValue::CALENDAR_GREGORIAN;
+		$localValue1955 = new TimeValue( '+1955-03-11T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_DAY, $g );
+		$localValue2015 = new TimeValue( '+2015-00-00T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_YEAR, $g );
+		$localValue2016 = new TimeValue( '+2016-03-00T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_MONTH, $g );
 
 		return array(
-			// Matches
-			array(
+			'Same second' => array(
+				ComparisonResult::STATUS_MATCH,
+				new TimeValue( '+2015-01-01T01:01:01Z', 0, 0, 0, TimeValue::PRECISION_SECOND, $g ),
+				new TimeValue( '+2015-01-01T01:01:01Z', 0, 0, 0, TimeValue::PRECISION_SECOND, $g )
+			),
+			'Same day' => array(
 				ComparisonResult::STATUS_MATCH,
 				$localValue1955,
-				new TimeValue( '+0000000000001955-03-11T00:00:00Z', 0, 0, 0, 11, 'http://www.wikidata.org/entity/Q1985727' )
+				new TimeValue( '+1955-03-11T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_DAY, $g )
 			),
-			array(
+			'Other day' => array(
 				ComparisonResult::STATUS_MISMATCH,
 				$localValue1955,
-				new TimeValue( '+0000000000001991-05-23T00:00:00Z', 0, 0, 0, 11, 'http://www.wikidata.org/entity/Q1985727' )
+				new TimeValue( '+1991-05-23T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_DAY, $g )
 			),
-			array(
+			'Same year' => array(
 				ComparisonResult::STATUS_MATCH,
 				$localValue2015,
-				new TimeValue( '+0000000000002015-00-00T00:00:00Z', 0, 0, 0, 9, 'http://www.wikidata.org/entity/Q1985727' )
+				new TimeValue( '+2015-00-00T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_YEAR, $g )
 			),
-			// Partial matches
-			array(
+			'Partial year vs. month match' => array(
 				ComparisonResult::STATUS_PARTIAL_MATCH,
 				$localValue2015,
-				new TimeValue( '+0000000000002015-03-00T00:00:00Z', 0, 0, 0, 10, 'http://www.wikidata.org/entity/Q1985727' )
+				new TimeValue( '+2015-03-00T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_MONTH, $g )
 			),
-			array(
+			'Partial month vs. year match' => array(
 				ComparisonResult::STATUS_PARTIAL_MATCH,
 				$localValue2016,
-				new TimeValue( '+0000000000002016-00-00T00:00:00Z', 0, 0, 0, 9, 'http://www.wikidata.org/entity/Q1985727' )
+				new TimeValue( '+2016-00-00T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_YEAR, $g )
 			),
-			array(
+			'Other year' => array(
 				ComparisonResult::STATUS_MISMATCH,
-				new TimeValue( '+0000000000011980-08-00T00:00:00Z', 0, 0, 0, 9, 'http://www.wikidata.org/entity/Q1985727' ),
-				new TimeValue( '+0000000000001980-00-00T00:00:00Z', 0, 0, 0, 9, 'http://www.wikidata.org/entity/Q1985727' )
-			)
+				new TimeValue( '+11980-08-00T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_YEAR, $g ),
+				new TimeValue( '+1980-00-00T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_YEAR, $g )
+			),
+			'Other gigayear' => array(
+				ComparisonResult::STATUS_MISMATCH,
+				new TimeValue( '+1000000000-00-00T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_YEAR1G, $g ),
+				new TimeValue( '+2000000000-00-00T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_YEAR1G, $g )
+			),
+			'Other decade with year precision' => array(
+				ComparisonResult::STATUS_MISMATCH,
+				new TimeValue( '+2010-00-00T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_YEAR, $g ),
+				new TimeValue( '+2020-00-00T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_YEAR, $g )
+			),
+			'Other year with month precision' => array(
+				ComparisonResult::STATUS_MISMATCH,
+				new TimeValue( '+2001-01-00T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_MONTH, $g ),
+				new TimeValue( '+2002-01-00T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_MONTH, $g )
+			),
+			'Other month with day precision' => array(
+				ComparisonResult::STATUS_MISMATCH,
+				new TimeValue( '+2001-01-01T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_DAY, $g ),
+				new TimeValue( '+2001-02-01T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_DAY, $g )
+			),
+			'Other day with hour precision' => array(
+				ComparisonResult::STATUS_MISMATCH,
+				new TimeValue( '+2001-01-01T01:00:00Z', 0, 0, 0, TimeValue::PRECISION_HOUR, $g ),
+				new TimeValue( '+2001-01-02T01:00:00Z', 0, 0, 0, TimeValue::PRECISION_HOUR, $g )
+			),
+			'Other hour with minute precision' => array(
+				ComparisonResult::STATUS_MISMATCH,
+				new TimeValue( '+2001-01-01T01:01:00Z', 0, 0, 0, TimeValue::PRECISION_MINUTE, $g ),
+				new TimeValue( '+2001-01-01T02:01:00Z', 0, 0, 0, TimeValue::PRECISION_MINUTE, $g )
+			),
+			'Other minute with second precision' => array(
+				ComparisonResult::STATUS_MISMATCH,
+				new TimeValue( '+2001-01-01T01:01:01Z', 0, 0, 0, TimeValue::PRECISION_SECOND, $g ),
+				new TimeValue( '+2001-01-01T01:02:01Z', 0, 0, 0, TimeValue::PRECISION_SECOND, $g )
+			),
+			// FIXME: This is a bug!
+			'Can not compare 5+ digit years' => array(
+				ComparisonResult::STATUS_MISMATCH,
+				new TimeValue( '+12345-00-00T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_YEAR, $g ),
+				new TimeValue( '+12345-00-00T00:00:00Z', 0, 0, 0, TimeValue::PRECISION_YEAR, $g )
+			),
 		);
 	}
 
