98ba1f9dfa8daa26ed6c19f362d33a2d758314e3
WikidataBuilder <wikidata-services@wikimedia.de>
2016-03-08 10:00:25 +0000

    New Wikidata Build - 2016-03-08T10:00:01+0000
    Change-Id: I710087995b5ca255abb7007f4e09fb811da1c378

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,6 +23,16 @@ class ComposerAutoloaderInit_mediawiki_extension_wikidata
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit_mediawiki_extension_wikidata', 'loadClassLoader'));
 
+        $map = require __DIR__ . '/autoload_namespaces.php';
+        foreach ($map as $namespace => $path) {
+            $loader->set($namespace, $path);
+        }
+
+        $map = require __DIR__ . '/autoload_psr4.php';
+        foreach ($map as $namespace => $path) {
+            $loader->setPsr4($namespace, $path);
+        }
+
         $classMap = require __DIR__ . '/autoload_classmap.php';
         if ($classMap) {
             $loader->addClassMap($classMap);
@@ -32,19 +42,15 @@ class ComposerAutoloaderInit_mediawiki_extension_wikidata
         $loader->register(false);
 
         $includeFiles = require __DIR__ . '/autoload_files.php';
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire_mediawiki_extension_wikidata($fileIdentifier, $file);
+        foreach ($includeFiles as $file) {
+            composerRequire_mediawiki_extension_wikidata($file);
         }
 
         return $loader;
     }
 }
 
-function composerRequire_mediawiki_extension_wikidata($fileIdentifier, $file)
+function composerRequire_mediawiki_extension_wikidata($file)
 {
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-    }
+    require $file;
 }
