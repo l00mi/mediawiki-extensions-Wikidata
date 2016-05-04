a31fbb95b688d01d8a8bddb8d159317ae23e0ac2
aude <aude.wiki@gmail.com>
2016-05-04 19:29:47 -0400

    Update Wikidata - Fixes bug in "other languages" term box
    also fixed the Wikibase branch version from the accidentally
    created (and deleted) wmf/1.28.0-wmf.1 branch to wmf/1.27.0-wmf.23
    in the composer.json.
    
    Change-Id: Idccb8c9d78ba2e41522281f2182268f8c5559ce1

--- WikibaseClient.settings.php
+++ WikibaseClient.settings.php
@@ -1,2 +1,2 @@
 <?php
-$wgWBClientSettings["sharedCacheKeyPrefix"] = "wikibase_shared/wikidata_1_28_0_wmf_1";
+$wgWBClientSettings["sharedCacheKeyPrefix"] = "wikibase_shared/wikidata_1_27_0_wmf_23";
