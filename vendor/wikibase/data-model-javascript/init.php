6b015bf375d902af6ff35db5964aa4d4e13ab797
WikidataBuilder <wikidata-services@wikimedia.de>
2016-09-10 10:00:24 +0000

    New Wikidata Build - 2016-09-10T10:00:01+0000
    Change-Id: Ide47b7ae20ded88e69aa225e1e84252c4b5a349d

--- vendor/wikibase/data-model-javascript/init.php
+++ vendor/wikibase/data-model-javascript/init.php
@@ -3,11 +3,11 @@
 /**
  * @deprecated since 3.0
  */
-define( 'WIKIBASE_DATAMODEL_JAVASCRIPT_VERSION', '3.0.0' );
+define( 'WIKIBASE_DATAMODEL_JAVASCRIPT_VERSION', '3.0.1' );
 
 if ( defined( 'MEDIAWIKI' ) && function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'WikibaseDataModelJavaScript', __DIR__ . '/mediawiki-extension.json' );
 
-	include 'resources.php';
-	include 'resources.test.php';
+	include __DIR__ . '/resources.php';
+	include __DIR__ . '/resources.test.php';
 }
